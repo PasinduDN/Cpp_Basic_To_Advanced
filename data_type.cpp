b6ebd28a54439c34e2b7cf55cedbdc56a3996c2d
Pasindu Dulanjana <pasindusl07@gmail.com>
2023-04-19 12:57:34 +0530

    Create data_type.cpp

---
+++ data_type.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int mynumber=5;
+    float myfloatnum=5.99;
+    double mydoublenum=9.98;
+    char myletter='D';
+    bool myboolean= true;
+    string mystring="Hello";
+
+    cout<< "Int: " << mynumber << endl;
+    cout<< "Float: " << myfloatnum << endl;
+    cout<< "double: " << mydoublenum << endl;
+    cout<< "char: " << myboolean << endl;
+    cout<< "bool: " << myboolean << endl;
+    cout<< "string: " << mystring << endl;
+}
