cb0b461efe8735a3a32c2c3d92eb0d45c5692a75
Pasindu Dulanjana <pasindusl07@gmail.com>
2023-04-19 10:16:37 +0530

    Create comment.cpp

---
+++ comment.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+
+  // This is single line Comment
+
+  /* This is 
+  Multi - Line 
+  Comment */
+  
+  return 0;
+}
