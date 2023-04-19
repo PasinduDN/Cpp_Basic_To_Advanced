5f524d9e0617d0f578693b606e7eb9e41595baf1
Pasindu Dulanjana <pasindusl07@gmail.com>
2023-04-19 21:14:26 +0530

    Create string_length.cpp

---
+++ string_length.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string txt="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    cout<<"The Length Of The tct String is: " << txt.size();
+    return 0;
+
+}  
