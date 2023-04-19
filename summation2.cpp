30888611e33061d0d864a9c594832b582a4b3fa3
Pasindu Dulanjana <pasindusl07@gmail.com>
2023-04-19 12:23:37 +0530

    Create summation2.cpp

---
+++ summation2.cpp
@@ -0,0 +1,12 @@
+//Getting the sum of two values
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x=5;
+    int y=10;
+    int sum=x+y;
+    cout << "Summation Is: "<<  sum;
+    return 0;
+
+}
